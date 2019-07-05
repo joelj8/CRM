cfea750fea9161d9e5e4709407445dbd19dcd6fe
joelj8 <joel.j8@gmail.com>
2019-07-05 15:41:16 -0400

    First commit CRMAPP

---
+++ CRM.UI/Controllers/CustomersController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRM.Data;
+using CRM.Models;
+
+namespace CRM.UI.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private CDBContext db = new CDBContext();
+
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            SelectList lsGender = new SelectList(db.genders, "genderId", "genderName");
+            ViewBag.GenderList = lsGender;
+
+            return View(db.customers.ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            
+            SelectList lsGender = new SelectList(db.genders, "genderId", "genderName");
+
+            ViewBag.GenderList = lsGender;
+            
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Phone,Mail,FechaNacimiento,Address,GenderId")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            SelectList lsGender = new SelectList(db.genders, "genderId", "genderName");
+            ViewBag.GenderList = lsGender;
+
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Phone,Mail,Address,FechaNacimiento,GenderId")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            SelectList lsGender = new SelectList(db.genders, "genderId", "genderName");
+            ViewBag.GenderList = lsGender;
+
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.customers.Find(id);
+            db.customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CargaData(string nombre, string genero)
+        {
+            var cusList = db.customers.ToList<Customer>();
+           if (genero != string.Empty)
+            {
+                cusList = db.customers.Where(c => c.GenderId.Contains(genero.Trim())).ToList();
+            }
+            
+            return Json(new { data = cusList }, JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CRM.UI/Scripts/jsGeneral.js
@@ -0,0 +1,206 @@
+﻿
+$(document).ready(function() {
+        // Mover la tabla al espacio deseado
+        //$("#divTabla").css({ top: 40, left: 50, position: 'absolute' });
+
+            // Configurando la variable para cambiar el idioma 
+            var spanish ={"sProcessing": "Procesando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+		        "oPaginate": {
+                "sFirst": "Primero",
+            "sLast": "Último",
+            "sNext": "Siguiente",
+            "sPrevious": "Anterior"
+            },
+		        "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }};
+
+    debugger
+    //Configurando el DataTable para la Tabla HTML
+    $('#GridFiltroX').DataTable({
+            lengthMenu: [ [5, 15, 25, -1], [5, 15, 25, "All"] ],
+            language: spanish,
+            dom: 'Blfrtip',
+            buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+    });
+
+    $("#idGenero").change(function () {
+        cargadatagrid();
+    });
+
+    cargadatagrid();
+
+    //Ocultando los botonos por defecto
+    HideButtons();
+});
+
+
+
+function cargadatagridv2() {
+    $("#GridFiltro tbody tr").remove();
+    var dataparam = {
+        "nombre": "joel",
+        "genero": $("#idGenero").val()
+    };
+    $.ajax({
+        "url": "Customers/CargaData",
+        "data": dataparam,
+        "tye": "GET",
+        "datatype": "json",
+        "contentType": "application/json; charset=utf-8",
+        success: function (data) {
+            debugger
+            $("#GridFiltro tbody tr").remove();
+            //$('#GridFiltro').append('<tr><td>Columna 1.1</td></tr>');
+
+            $.each(data["data"], function (i, item) {
+                $('#GridFiltro tbody').append('<tr> <td>' + item.Name + '</td> <td>' + item.Phone + '</td > <td>' + item.Mail + '</td> <td>' + item.Address + '</td> <td>' + item.GenderGrl.GenderName + '</td> </tr> ');
+            });
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+
+
+}
+
+    function cargadatagrid() {
+    debugger
+    var spanish = {
+        "sProcessing": "Procesando...",
+        "sLengthMenu": "Mostrar _MENU_ registros",
+        "sZeroRecords": "No se encontraron resultados",
+        "sEmptyTable": "Ningún dato disponible en esta tabla",
+        "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sSearch": "Buscar:",
+        "sUrl": "",
+        "sInfoThousands": ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst": "Primero",
+            "sLast": "Último",
+            "sNext": "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "oAria": {
+            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }
+    };
+
+    var dataparam = {
+        "nombre": "joel",
+        "genero": $("#idGenero").val()
+        };
+        var urlAjax = "@Url.Action('Customers', 'CargaData')";
+        urlAjax = "./Customers/CargaData";
+        var myUrl = $("#myUrl").val();
+        //alert(myUrl);
+        //urlAjax = '@(Url.Action("CargaData","Customers"))';
+
+    $("#GridFiltro").DataTable({
+        lengthMenu: [[ 15, 25, 50, -1], [ 15, 25, 50, "All"]],
+        language: spanish,
+        dom: 'Blfrtip',
+        buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
+        destroy: true,
+        
+        "ajax": {
+            "processing": true,
+            "url": MyAppUrlSettings.MyUsefulUrl,
+            "cache": false,
+            "data": dataparam,
+            "tye": "GET",
+            "datatype": "json"
+        },
+        "columnDefs": [
+            {
+                "targets": [5], "data": "id", "render": function (data, type, row, meta) {
+                    return '<a href="\Customers/Edit/replace" class= "btn btn-xs btn-primary" ><span class="glyphicon glyphicon-pencil"></span> </a>'.replace("replace", row.ID) + ' '
+                        + '<a href="\Customers/Delete/replace" class= "btn btn-xs btn-danger" ><span class="glyphicon glyphicon-trash"></span> </a>'.replace("replace", row.ID);
+                }
+            }
+            
+        ],
+        "columns": [
+            { "data": "Name", "autowidth": true },
+            { "data": "Phone", "autowidth": true },
+            { "data": "Mail", "autowidth": true },
+            { "data": "Address", "autowidth": true },
+            { "data": "GenderGrl.GenderName", "autowidth": true }
+        ]
+
+    });    
+
+    HideButtons();
+    
+}
+
+
+    function clickExcel() {
+        //alert("Click Excel");
+        $("#btnExcel").click();
+    }
+
+	function clickPDF(){
+        //alert("Click Excel");
+        $("#btnPDF").click();
+    }
+
+    function HideButtons(){
+	    var buttonsAll = $("button");
+	    for (var i = 0; i < buttonsAll.length; i++){
+                let buttonEvalua = buttonsAll[i].className;
+
+            // Boton de Copy
+            if (buttonEvalua.includes("copy")){
+                buttonsAll[i].id = "btnCopy";
+               $("#btnCopy").hide();
+            }
+
+            // Boton de Excel
+	        if (buttonEvalua.includes("excel")){
+                buttonsAll[i].id = "btnExcel";
+                $("#btnExcel").hide();
+             }
+
+            // Boton de CSV
+	        if (buttonEvalua.includes("csv")){
+                buttonsAll[i].id = "btnCSV";
+                $("#btnCSV").hide();
+            }
+
+             // Boton de PDF
+	        if (buttonEvalua.includes("pdf")){
+                buttonsAll[i].id = "btnPDF";
+                $("#btnPDF").hide();
+            }
+
+            // Boton de Print
+	        if (buttonEvalua.includes("print")){
+                buttonsAll[i].id = "btnPrint";
+                $("#btnPrint").hide();
+            }
+
+        }
+    }
+
+
+
